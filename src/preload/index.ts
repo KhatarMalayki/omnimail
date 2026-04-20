3d75310c8ceeb136f406cb9c5e527c29aafa7e75
KhatarMalayki <104155918+KhatarMalayki@users.noreply.github.com>
2026-04-20 04:07:06 -0400

    feat: integrate API bridge into preload

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,15 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
-
-// Custom APIs for renderer
-const api = {
-  // Database & Email APIs will be exposed here
-  ping: () => ipcRenderer.send('ping'),
-  
-  // Example for future database operations
-  // getAccounts: () => ipcRenderer.invoke('db:get-accounts'),
-  // getMessages: (folderId: string) => ipcRenderer.invoke('db:get-messages', folderId),
-};
+import './api';
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
@@ -17,13 +8,10 @@ const api = {
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI);
-    contextBridge.exposeInMainWorld('api', api);
   } catch (error) {
     console.error(error);
   }
 } else {
   // @ts-ignore (define in d.ts)
   window.electron = electronAPI;
-  // @ts-ignore (define in d.ts)
-  window.api = api;
 }
